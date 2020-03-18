7bd7e87381dc24ec61e9fa746e88496abbc46ca8
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2020-03-18 16:42:16 +0600

    Update 2 declare variables.cpp

--- cpp programs foder/2 declare variables.cpp
+++ cpp programs foder/2 declare variables.cpp
@@ -9,6 +9,19 @@ int main()
     int num1;
     num1 = 10;
     
+    double num2 = 5.39;
+    
+    char letter = 'd';
+    
+    string sentence = "I am a student";
+    
+    bool boolean_logic = "TRUE";
+    
     cout << num1 << endl;
+    cout << num2 << endl;
+    cout << letter << endl;
+    cout << sentence << endl;
+    cout << boolean_logic << endl;
+    
     return 0;
 }
