544657bf118ade2453651ca50885f748e33e1863
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2020-03-18 16:36:20 +0600

    Create 2 declare variables.cpp

---
+++ cpp programs foder/2 declare variables.cpp
@@ -0,0 +1,14 @@
+// this is comment section
+// variable declaration
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num1;
+    num1 = 10;
+    
+    cout << num1 << endl;
+    return 0;
+}
